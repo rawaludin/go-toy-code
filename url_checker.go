f45edfb9c1ba0d804abb62090234dfb631cb7e2b
Rahmat Awaludin <rahmat.awaludin@gmail.com>
2018-12-16 17:33:30 +0700

    added url_checker

--- url_checker.go
+++ url_checker.go
@@ -11,11 +11,11 @@ func main() {
 		"http://in va lid.com",
 		"http://www.facebook.com",
 	}
+
 	for _, u := range urls {
 		valid, err := url.Parse(u)
 		if err != nil {
-			fmt.Println(u)
-			fmt.Println(err)
+			fmt.Printf("%#v\n", err)
 			continue
 		}
 		fmt.Println(valid)
