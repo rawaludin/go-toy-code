60f225777ee038f1610be76114a7eeb69945501d
Rahmat Awaludin <rahmat.awaludin@gmail.com>
2018-12-16 18:54:55 +0700

    added sleepy

---
+++ sleepy.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+func main() {
+	c := make(chan int)
+	for i := 0; i < 5; i++ {
+		go sleepyGopher(i, c)
+	}
+
+	timeout := time.After(2 * time.Second)
+	for i := 0; i < 5; i++ {
+		select {
+		case gopherID := <-c:
+			fmt.Println("gopher ", gopherID, " has finished sleeping")
+		case <-timeout:
+			fmt.Println("my patience ran out")
+			return
+		}
+	}
+
+	time.Sleep(4 * time.Second)
+}
+
+func sleepyGopher(id int, c chan int) {
+	time.Sleep(time.Duration(rand.Intn(4000)) * time.Millisecond)
+	fmt.Println("... ", id, " snore ...")
+	c <- id
+}
